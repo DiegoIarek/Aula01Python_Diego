fabea9767f4197a875ee7b87e78a47c65e631e6c
Arthur Martins <inacio.arthur@escola.pr.gov.br>
2024-03-27 10:40:16 -0300

    atividade 1 de python

---
+++ aula01.py
@@ -0,0 +1,11 @@
+import os
+
+#print("Olá mundo cruel!")
+print("o 3DS é a turma número 1 do sagrada")
+turma="3DS"
+pos=1
+
+print ("o" , turma, "é a turma número", pos, "do Sagrada")
+print(type(turma))
+print(type(pos))
+#os.system("clear")
